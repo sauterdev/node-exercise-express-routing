ddd519525c1a96b5e0ff3716f8c54509b20e239a
Chris Sauter <sauterdev@gmail.com>
2023-02-10 15:01:06 -0500

    node-express-route exercise

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import config from "./config";
 // TODO: import router from routes/
+import router from "./routes/index.js";
 
 const app = express();
 
+//Middleware
+
 app.use(express.json());
+app.use("/api", router)
 
 // TODO: use the imported router to handle all requests
 
+
+
+
+//error handler
 app.use((err, req, res, next) => {
   console.error(err);
   res.json({ name: err.name, msg: err.message });
